65b7ca1610100fd0b65f2cdd9a4f74c0492b2ac7
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-12-21 15:55:58 -0300

    Create BibliotecaLiteraluraApplication.java

---
+++ biblioteca/src/main/java/com/challenge/biblioteca/BibliotecaLiteraluraApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaLiteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaLiteraluraApplication.class, args);
+	}
+
+}
