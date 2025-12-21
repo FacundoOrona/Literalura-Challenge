288115a90fe46d689f888bddb4fb3002f41ab388
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-12-21 16:37:11 -0300

    Add Jackson annotations to RespuestaGutendex model
    Introduced @JsonAlias for mapping JSON fields to class properties and @JsonIgnoreProperties to ignore unknown fields during deserialization. Also added a toString() override for better object representation.

--- biblioteca/src/main/java/com/challenge/biblioteca/model/RespuestaGutendex.java
+++ biblioteca/src/main/java/com/challenge/biblioteca/model/RespuestaGutendex.java
@@ -1,9 +1,15 @@
 package com.challenge.biblioteca.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RespuestaGutendex {
+    @JsonAlias("count")
     private int total;
+    @JsonAlias("results")
     private List<LibroDto> resultados;
 
     public int getTotal() { return total; }
@@ -11,5 +17,11 @@ public class RespuestaGutendex {
 
     public List<LibroDto> getResultados() { return resultados; }
     public void setResultados(List<LibroDto> resultados) { this.resultados = resultados; }
+
+    @Override
+    public String toString() {
+        return "total: " + total +
+                ", resultados: " + resultados;
+    }
 }
 
