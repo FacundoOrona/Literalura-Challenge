98156166b729c3a3f2a273dd81db43b0494b0310
FacundoOrona <fcndorona@gmail.com>
2025-12-24 11:36:38 -0300

    Update BibliotecaLiteraluraApplication.java

--- biblioteca/src/main/java/com/challenge/biblioteca/BibliotecaLiteraluraApplication.java
+++ biblioteca/src/main/java/com/challenge/biblioteca/BibliotecaLiteraluraApplication.java
@@ -1,17 +1,22 @@
 package com.challenge.biblioteca;
 
+import com.challenge.biblioteca.model.Autor;
+import com.challenge.biblioteca.model.Libro;
 import com.challenge.biblioteca.service.LibroServicio;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+import java.util.Scanner;
+
 @SpringBootApplication
 public class BibliotecaLiteraluraApplication implements CommandLineRunner {
 
-    private final LibroServicio libroServicio;
+    private final LibroServicio servicio;
 
-    public BibliotecaLiteraluraApplication(LibroServicio libroServicio) {
-        this.libroServicio = libroServicio;
+    public BibliotecaLiteraluraApplication(LibroServicio servicio) {
+        this.servicio = servicio;
     }
 
     public static void main(String[] args) {
