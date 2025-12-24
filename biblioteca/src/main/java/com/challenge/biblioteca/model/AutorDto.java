edc08c7e3e7b0e2b7b35aee89ce34da36e9105d6
FacundoOrona <fcndorona@gmail.com>
2025-12-24 11:37:24 -0300

    Update AutorDto.java

--- biblioteca/src/main/java/com/challenge/biblioteca/model/AutorDto.java
+++ biblioteca/src/main/java/com/challenge/biblioteca/model/AutorDto.java
@@ -9,6 +9,12 @@ public class AutorDto {
     @JsonAlias("name")
     private String nombre;
 
+    @JsonAlias("birth_year")
+    private Integer anioNacimiento;
+
+    @JsonAlias("death_year")
+    private Integer anioFallecimiento;
+
     public String getNombre() {
         return nombre;
     }
@@ -17,9 +23,27 @@ public class AutorDto {
         this.nombre = nombre;
     }
 
+    public Integer getAnioNacimiento() {
+        return anioNacimiento;
+    }
+
+    public void setAnioNacimiento(Integer anioNacimiento) {
+        this.anioNacimiento = anioNacimiento;
+    }
+
+    public Integer getAnioFallecimiento() {
+        return anioFallecimiento;
+    }
+
+    public void setAnioFallecimiento(Integer anioFallecimiento) {
+        this.anioFallecimiento = anioFallecimiento;
+    }
+
     @Override
     public String toString() {
-        return "Autor: " + nombre;
+        return nombre + " (" + 
+               (anioNacimiento != null ? anioNacimiento : "¿?") + " - " + 
+               (anioFallecimiento != null ? anioFallecimiento : "vivo") + ")";
     }
-    
 }
+
